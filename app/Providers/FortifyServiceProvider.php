d69a1eaf5d9dec1acf753c0c5cb0a159d1ce385f
BM <monnybram@outlook.com>
2026-05-21 18:00:21 +0900

    Add Reset Password $ Forgot Password

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -48,9 +48,17 @@ class FortifyServiceProvider extends ServiceProvider
         Fortify::registerView(function() {
             return view('auth.register');
         });
-    
+
         Fortify::loginView(function() {
             return view('auth.login');
+        });
+
+        Fortify::requestPasswordResetLinkView(function () {
+            return view('auth.forgot-password');
+        });
+
+        Fortify::resetPasswordView(function ($request) {
+            return view('auth.reset-password', ['request' => $request]);
         });
 
         RateLimiter::for('passkeys', function (Request $request) {
